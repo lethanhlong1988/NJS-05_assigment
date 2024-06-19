b5323385a8a15b01c7e967326cddb002cdf3deb7
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-06-19 07:22:36 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
+router.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
+
 router.get("/users", (req, res, next) => {
-    console.log("/users middleware");
-    res.send("<p>The Middleware that handles just /users</p>");
-  });
-  
-  router.get("/", (req, res, next) => {
-    console.log("/ middleware");
-    res.send("<p>The Middleware that handles just</p>");
-  });
+  res.sendFile(path.join(__dirname, "..", "views", "users.html"));
+});
 
 module.exports = router;
